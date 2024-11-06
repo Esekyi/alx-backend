10c83938e2aaee543cdedf76fde01bc718e63e6a
[esekyi] <[sskert10@gmail.com]>
2024-11-06 04:19:47 +0000

    1. Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Basic Babel setup"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Config app class"""
+
+    LANGUAGES = ["en", "fr"]
+
+
+# def get_locales():
+#     """Get languages from Config class"""
+#     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, default_locale='en', default_timezone='UTC',)
+
+
+if __name__ == '__main__':
+    app.run()
