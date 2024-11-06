3802c7e054100a05dd8c9d586a8ebfddac89cf27
[esekyi] <[sskert10@gmail.com]>
2024-11-06 04:48:56 +0000

    [fixes🛠️] configured babel defaults

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Babel setup"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
@@ -8,16 +8,13 @@ class Config:
     """Config app class"""
 
     LANGUAGES = ["en", "fr"]
-
-
-# def get_locales():
-#     """Get languages from Config class"""
-#     return request.accept_languages.best_match(app.config['LANGUAGES'])
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app, default_locale='en', default_timezone='UTC',)
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
@@ -27,4 +24,5 @@ def index() -> str:
 
 
 if __name__ == '__main__':
+    """Main function"""
     app.run()
