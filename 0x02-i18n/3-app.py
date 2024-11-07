f6130fa3f6ed4d7144b29967e19df94d573a83a9
[esekyi] <[sskert10@gmail.com]>
2024-11-07 05:22:59 +0000

    fixes - parametrize and set defaults to en

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-"""Parametrize templates"""
+"""Parametrize templates part 1"""
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 from typing import Union
 
 
 class Config:
-    """Config app class"""
+    """Config app class with default babel values"""
 
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "fr"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
@@ -20,18 +20,18 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> Union[str, None]:
-    """Get locale from a request"""
+    """Get locale language from a request"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    """Return an html file"""
+    """Return an html file and parametrize it"""
     return render_template('3-index.html',
                            title=_("home_title"),
                            header=_("home_header"))
 
 
 if __name__ == '__main__':
-    """Main function"""
+    """Main function to run the app"""
     app.run()
