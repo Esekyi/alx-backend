29b9da307cde43ba6ff406dadef987a70084f368
[esekyi] <[sskert10@gmail.com]>
2024-11-09 06:16:40 +0000

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,7 +2,7 @@
 """Mock logging in."""
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
-from typing import Union, Dict
+from typing import Union, Dict, Optional
 
 
 _.__doc__ = "Alias for `gettext`, used to mark strings for translation."
@@ -28,39 +28,57 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-def get_user(user: int) -> Union[Dict, None]:
+@babel.localeselector
+def get_locale() -> Union[str, None]:
+    """
+    Determine the best match language for the current request.
+
+    First checks if a 'locale' parameter is provided in the URL and matches
+    a supported language. If so, uses it
+    otherwise, defaults to the client's
+    preferred language based on 'Accept-Language' headers.
+    """
+    locale = request.args.get('locale')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+def get_user() -> Optional[Dict]:
     """
     User login function
     params: ID
     returns: a dictionary of users or None
     """
-    if user in users:
-        return users[user]
-    return None
+    try:
+        user_id = int(request.args.get('login_as'))
+        return users.get(user_id)
+    except (TypeError, ValueError):
+        return None
 
 
 @app.before_request
-def before_request(userP=None):
+def before_request():
     """Before request function to handle user login."""
-    user = get_user(userP)
-    if user:
-        g.user = user
-    else:
-        g.user = None
+    g.user = get_user()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
-def index():
-    """Index page."""
+def index() -> str:
+    """
+    Render the main index page with a message depending on the login state.
+
+    Returns:
+        str: Rendered HTML template.
+    """
     if g.user:
-        user = g.user.get("name")
+        welcome_msg = _("logged_in_as", username=g.user.get("name"))
     else:
-        user = None
-    return render_template("5-index.html", user=user,
+        welcome_msg = _("not_logged_in")
+    return render_template("5-index.html", welcome_msg=welcome_msg,
                            title=_("home_title"),
-                           header=_("home_header"),
-                           logged_in_as=_("logged_in_as"),
-                           not_logged_in=_("not_logged_in"))
+                           header=_("home_header"))
+
 
 if __name__ == '__main__':
     """Main function to run the app"""
